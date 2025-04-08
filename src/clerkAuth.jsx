79966667adea03edb2cbf28b80aff6ef1f35a784
Dinakar S <maziyadinakar@gmail.com>
2025-04-08 13:20:01 +0530

    Delete src directory

--- src/clerkAuth.jsx
+++
@@ -1,7 +0,0 @@
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const clerkPublicKey = "pk_test_c2VjdXJlLWpheS01OS5jbGVyay5hY2NvdW50cy5kZXYk"; // Replace with actual key
-
-export default function ClerkAuthProvider({ children }) {
-  return <ClerkProvider publishableKey={clerkPublicKey}>{children}</ClerkProvider>;
-}
